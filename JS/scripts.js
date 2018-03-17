0ae2f2caf54f4822636d3a9fe02ca60c45c865d3
AidanM8 <37283163+AidanPTD@users.noreply.github.com>
2018-03-16 18:08:46 -0700

    Adding index.html

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+function sideBar_Open() {
+    document.getElementById("mySidebar").style.display="block";
+    document.getElementById("myOVerlay").style.display="block";
+}
+function sideBar_Close() {
+    document.getElementById("mySidebar").style.display="none";
+    document.getElementById("myOVerlay").style.display="none";
+}
