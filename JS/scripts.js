8d2845f0f639b3e1d602fa4a80bb86cbdfc193bf
AidanM8 <37283163+AidanPTD@users.noreply.github.com>
2018-03-16 19:32:00 -0700

    Add nav bar

--- JS/scripts.js
+++
@@ -1,8 +0,0 @@
-function sideBar_Open() {
-    document.getElementById("mySidebar").style.display="block";
-    document.getElementById("myOVerlay").style.display="block";
-}
-function sideBar_Close() {
-    document.getElementById("mySidebar").style.display="none";
-    document.getElementById("myOVerlay").style.display="none";
-}
